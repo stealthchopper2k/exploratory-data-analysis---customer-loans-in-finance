3e59e217f1ee4f46936d8632cb32a3ec923a7033
stealthchopper2k <stealthchopper2k@gmail.com>
2023-10-30 15:26:52 +0000

    Finished Transformations of columns to appropriate data types

--- load.py
+++ load.py
@@ -12,27 +12,4 @@ def load_data(file_path):
 
 df = load_data("/dataset/loan_data.csv")
 print(df.dtypes)
-
-# print(dt)
-
-# Are there columns that could be represented better numerically, are dates in the correct format, should some columns be categorical? Are there any excess symbols in the data?
-
-# Unnamed data remove
-# loan_amount, never need decimal place?
-# funded amount, decimal place?
-# term - obj -> date
-# installment
-# loan grade -> string
-# employment length - object -> months?
-# ownership -> type -> string
-# issue date -> obj -> date
-# loan_status -> NEEDS MORE LOOKS
-# payment_plan -> boolean
-# delinq_2yrs fine int for month
-# earliest credit_line object to date
-# 'mths_since_last_delinq' -> INT and trim
-#  last_payment_date
-# ext_payment_date
-# last_credit_pull_date
-# application_type need?
-# grade, subgrade, purpose = categorical
+print(df.info())
--- transform.py
+++ transform.py
@@ -1,5 +1,6 @@
 import pandas as pd
 # from dateutil.parser import parse
+import numpy as np
 
 
 class DataTransform:
@@ -9,6 +10,7 @@ class DataTransform:
     def string_to_boolean(self, col_name):
         mask = {'n': False, 'y': True}
         self.df[col_name].map(mask)
+        self.df[col_name] = self.df[col_name].astype('bool')
         print(self.df[col_name].unique())
 
     def cols_to_categories(self, array):
@@ -19,9 +21,12 @@ class DataTransform:
         for col in array:
             self.df[col] = pd.to_datetime(self.df[col], format="%b-%Y")
 
-    def numerical_cols(self, cols):
+    def extract_num_from_string(self, cols):
         for col in cols:
             self.df[col] = self.df[col].str.extract('(\d+)')
+
+    def numerical_cols(self, cols):
+        for col in cols:
             pd.to_numeric(self.df[col])
 
     def rename(self, col_name, new_col_name):
@@ -29,21 +34,23 @@ class DataTransform:
 
     def drop_cols(self, cols):
         for col in cols:
-            self.df.drop(col, axis='columns')
+            self.df.drop(col, axis=1, inplace=True)
+
+    # despite type coercsion we might want to convert explicitly if exporting to excel for cleanliness
+    def to_int(self, cols):
+        for col in cols:
+            self.df[col] = self.df[col].fillna(0).astype('int8')
 
 
 if __name__ == '__main__':
     df = pd.read_csv('./dataset/loan_data.csv')
     Transformer = DataTransform(df)
 
-    cols_to_drop = ['application_type', 'policy_code']
-
-    Transformer.drop_cols(cols_to_drop)
-
+    # Convert n and y to bool values
     Transformer.string_to_boolean('payment_plan')
 
     categories = ['grade', 'sub_grade', 'home_ownership',
-                  'verification_status', 'loan_status', 'purpose', 'term']
+                  'verification_status', 'loan_status', 'purpose', 'employment_length']
 
     Transformer.cols_to_categories(categories)
 
@@ -52,9 +59,29 @@ if __name__ == '__main__':
 
     Transformer.strings_to_dates(string_dates)
 
-    numerical_cols = ['employment_length', 'term']
+    # month and year terms to int
+    string_to_num_cols = ['term']
+    numerical_cols = ['term', 'mths_since_last_record',
+                      'mths_since_last_major_derog', 'mths_since_last_delinq', 'mths_since_last_record']
 
     Transformer.rename('employment_length', 'Years Employed')
     Transformer.rename('term', 'Loan Months')
 
+    Transformer.extract_num_from_string(string_to_num_cols)
     Transformer.numerical_cols(numerical_cols)
+
+    # funded_amount seems to have more "nulls" according to .info() meaning that it has missing unexplained values that funded_amount_inv picks up on
+    # application and policy code have all same values across whole column
+    # out_prncp_inv/total_payment_inv is the same as out_prncp/total_payment
+    drop_cols = ['funded_amount', 'application_type',
+                 'policy_code', 'out_prncp_inv', 'total_payment_inv', 'Unnamed: 0', 'id']
+
+    Transformer.drop_cols(drop_cols)
+
+    Transformer.to_int(['mths_since_last_record', 'mths_since_last_major_derog',
+                       'mths_since_last_delinq', 'mths_since_last_record', 'open_accounts', 'total_accounts', 'collections_12_mths_ex_med', 'delinq_2yrs', 'loan_amount'])
+
+    # decreases megabytes from 18.2 to 11
+    # would be a lot more impactful in larger datasets
+    print(Transformer.df.dtypes)
+    print(Transformer.df.info())
